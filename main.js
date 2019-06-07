0bf1d8db8ee34847d2d6e0acc274e1daaf12abeb
Misiek <McMisieck@users.noreply.github.com>
2019-06-07 14:47:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 var video = document.getElementById('video');
 
 // Get access to the camera!
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+if(navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
         video.srcObject = stream;
-        video.play();
+        //video.play();
     });
 }
 
